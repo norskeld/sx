86cbab483d6c67a63300431e36273edcf750ac7f
Vladislav Mamon <arcdeltax@gmail.com>
2020-06-06 16:58:54 +0500

    refactor(update): use built-in async readFile, writeFile & access
    BREAKING CHANGE: this bumps the required node version to >=10.x

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,40 +1,42 @@
-import { promisify } from 'util'
+import { readFile, writeFile, access } from 'fs/promises'
+import { bold } from 'ansi-colors'
+import { constants } from 'fs'
 import { resolve } from 'path'
-import fs from 'fs'
 
 import { name, description, version } from '../package.json'
 import { error, log } from '../src/utils/logger'
 import tsconfig from '../tsconfig.json'
 
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
-const exists = promisify(fs.exists)
-
 async function update(): Promise<void> {
   // Get the path to the package.js which contains actual name, description and version info
-  const pkgPath = resolve(process.cwd(), tsconfig.compilerOptions.outDir, 'package.js')
+  const pkgDir = resolve(process.cwd(), tsconfig.compilerOptions.outDir)
+  const pkgPath = resolve(pkgDir, 'package.js')
+
+  // Check if the package.js is present and accessible
+  try {
+    await access(pkgPath, constants.F_OK | constants.R_OK | constants.W_OK)
+  } catch {
+    error(`Couldn't locate ${bold(`package.js`)} at ${bold(pkgDir)} or it's not readable/writable.`)
+    process.exit(1)
+  }
 
   // Since this is a scoped package, we want to use the part after slash
   const [, actualName] = name.split('/')
 
   try {
-    if (await exists(pkgPath)) {
-      // Read the file content as a string
-      const content = await readFile(pkgPath, { encoding: 'utf8' })
+    // Read the file content as a string
+    const content = await readFile(pkgPath, { encoding: 'utf8' })
 
-      // Do replacements
-      const replaced = content
-        .replace(/__NAME__/g, actualName)
-        .replace(/__DESCRIPTION__/g, description)
-        .replace(/__VERSION__/g, version)
+    // Do replacements
+    const replaced = content
+      .replace(/__NAME__/g, actualName)
+      .replace(/__DESCRIPTION__/g, description)
+      .replace(/__VERSION__/g, version)
 
-      // Write content back
-      await writeFile(pkgPath, replaced, 'utf8')
+    // Write content back
+    await writeFile(pkgPath, replaced, 'utf8')
 
-      log(`Successfully updated name, description and version of the package.`)
-    } else {
-      error(`Couldn't locate the package.js at ${pkgPath}`)
-    }
+    log(`Successfully updated name, description and version of the package.`)
   } catch (message) {
     error(`Couldn't update name, description and version. Details: ${message}`)
     process.exit(1)
